595054598ef37bc8fb1c22181040ce4ef9dd25bf
Shourya Shukla <shouryashukla.oo@gmail.com>
2020-07-08 02:27:04 +0530

    Remove count_lines()
    Remove count_lines() and use '--count' in git rev-list instead.
    
    Quoting Dscho:
    > That's actually not necessary. `git rev-list --count` will give you a nice
    > number, no need to capture a potentially large amount of memory only to
    > count the lines.
    >
    > This may also make the patch obsolete that makes `count_lines()` public.

--- builtin/submodule--helper.c
+++ builtin/submodule--helper.c
@@ -1137,14 +1137,9 @@ static void generate_submodule_summary(struct summary_cb *info,
 			prepare_submodule_repo_env(&cp_rev_list.env_array);
 
 			argv_array_pushl(&cp_rev_list.args, "rev-list",
-					 "--first-parent", range, "--", NULL);
-			if (!capture_command(&cp_rev_list, &sb_rev_list, 0)) {
-				if (sb_rev_list.len)
-					total_commits = count_lines(sb_rev_list.buf,
-								    sb_rev_list.len);
-				else
-					total_commits = 0;
-			}
+					 "--first-parent", "--count", range, "--", NULL);
+			if (!capture_command(&cp_rev_list, &sb_rev_list, 0))
+				total_commits = atoi(sb_rev_list.buf);
 
 			free(range);
 			strbuf_release(&sb_rev_list);
