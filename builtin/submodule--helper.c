8ee78acc558fe7b4c6307cb85ae737c024338000
Shourya Shukla <shouryashukla.oo@gmail.com>
2020-07-08 15:15:20 +0530

    Improve the 'for-status' statements
    Improve the for-status statements by removing the unnecessary checks at
    some places. There still remains more room for improvememnt but itis
    confusing me a bit.
    
    Quote from Dscho:
    
    > + if (sub && p->status != 'A') {
    
    Good. The shell script version _always_ retrieved the `.ignore` config
    value, even if the `status` is `A`. Your version is much better.
    
    But why bother calling `submodule_from_path()` if the status is `A`?
    
    I could actually see the `const struct submodule *sub;` declaration be
    pulled out of this scope, and combining the `if (info->for_status &&
    p->status != 'A'), and the moving the assignment of `sub` into the `else
    if ((sub = submodule_from_path(r, &null_oid, p->sm_path)) &&
    sub->ignore)`.
    
    That would save us one entire indentation level.
    ----------------------------------------------------------------
    https://lore.kernel.org/git/nycvar.QRO.7.76.6.2007031712160.50@tvgsbejvaqbjf.bet/

--- builtin/submodule--helper.c
+++ builtin/submodule--helper.c
@@ -1169,26 +1169,23 @@ static void prepare_submodule_summary(struct summary_cb *info,
 			continue;
 		}
 
-		if (info->for_status) {
+		if (info->for_status && p->status != 'A') {
 			char *config_key;
 			const char *ignore_config = "none";
 			const char *value;
 			const struct submodule *sub = submodule_from_path(the_repository,
-									  &null_oid,
-									  p->sm_path);
+									  &null_oid, p->sm_path);
 
-			if (sub && p->status != 'A') {
-				config_key = xstrfmt("submodule.%s.ignore",
-						     sub->name);
-				if (!git_config_get_string_const(config_key, &value))
-					ignore_config = value;
-				else if (sub->ignore)
-					ignore_config = sub->ignore;
+			config_key = xstrfmt("submodule.%s.ignore",
+					     sub->name);
+			if (!git_config_get_string_const(config_key, &value))
+				ignore_config = value;
+			else if (sub && sub->ignore)
+				ignore_config = sub->ignore;
 
-				free(config_key);
-				if (!strcmp(ignore_config, "all"))
-					continue;
-			}
+			free(config_key);
+			if (!strcmp(ignore_config, "all"))
+				continue;
 		}
 
 		/* Also show added or modified modules which are checked out */
